923f4c83faab03d37798cc1f0742fd772fd36beb
pollenjp <polleninjp@gmail.com>
2025-06-13 08:57:05 +0900

    refactoring lint and gen

--- gen-yaml.sh
+++ gen-yaml.sh
@@ -8,10 +8,14 @@ set -eu -o pipefail
 script_path=$(realpath "${BASH_SOURCE[0]}")
 script_dir=$(dirname "$script_path")
 
-find "$script_dir" -name "*.jsonnet" | while read -r jsonnet_file; do
-  echo "Processing... $jsonnet_file"
-  dirpath=$(dirname "$jsonnet_file")
-  basename=$(basename "$jsonnet_file" .jsonnet)
-  out_file="${dirpath}/.${basename}.gen.yaml"
-  jsonnet "$jsonnet_file" | yq -p json -o yaml > "$out_file"
-done
+# shellcheck disable=SC2016
+find "$script_dir" -name "*.jsonnet" -print0 \
+  | xargs -P0 -0 -I{} bash -c \
+    ' set -eu -o pipefail
+      jsonnet_file="$1"
+      echo "Processing... $jsonnet_file"
+      dirpath=$(dirname "$jsonnet_file")
+      basename=$(basename "$jsonnet_file" .jsonnet)
+      out_file="${dirpath}/.${basename}.gen.yaml"
+      jsonnet "$jsonnet_file" | yq -p json -o yaml > "$out_file"
+    ' _ {}
--- lint-hash.sh
+++ lint-hash.sh
@@ -5,27 +5,32 @@ set -euo pipefail
 hashed_sign=$(jsonnet -e '(import "jsonnetlib/hash.libsonnet") { data: "x" }.hashed_sign' | jq -r)
 [[ -z "$hashed_sign" ]] && echo "Error: hashed_sign is empty" && exit 1
 
-# Find all jsonnet files
-find . -name "*.jsonnet" | while read -r jsonnet_file; do
-  echo "Checking $jsonnet_file..."
+script_content=$(
+  cat <<__EOF__
+set -eu -o pipefail
 
-  # Parse jsonnet and check ConfigMap names
-  jsonnet "$jsonnet_file" | jq -r '
-    if type == "array" then .[] else . end |
-    select(
-      .kind == "ConfigMap"
-      or .kind == "PersistentVolume"
-      or .kind == "PersistentVolumeClaim"
-      or .kind == "OnePasswordItem"
-    ) |
-    .metadata.name |
-    select(test("'"$hashed_sign"'$") | not)
-  ' | while read -r invalid_name; do
-    if [[ -n "$invalid_name" ]]; then
-      echo "Error: ConfigMap '$invalid_name' in $jsonnet_file does not match the hashed sign. Use 'hash.libsonnet' to generate the name."
-      exit 1
-    fi
-  done
+jsonnet_file="\$1"
+echo "Checking \$jsonnet_file..."
+
+# Parse jsonnet and check ConfigMap names
+jsonnet "\$jsonnet_file" | jq -r '
+  if type == "array" then .[] else . end |
+  select(
+    .kind == "ConfigMap"
+    or .kind == "PersistentVolume"
+    or .kind == "PersistentVolumeClaim"
+    or .kind == "OnePasswordItem"
+  ) |
+  .metadata.name |
+  select(test("'"$hashed_sign"'$") | not)
+' | while read -r invalid_name; do
+  if [[ -n "\$invalid_name" ]]; then
+    echo "Error: ConfigMap '\$invalid_name' in \$jsonnet_file does not match the hashed sign. Use 'hash.libsonnet' to generate the name."
+    exit 1
+  fi
 done
+__EOF__
+)
 
-echo "All ConfigMap names are valid!"
+# Find all jsonnet files
+find . -name "*.jsonnet" -print0 | xargs -P0 -0 -I{} bash -c "$script_content" _ {}
